8b59f83fecaafeb28d53a8c875be48ffa58336f9
turchan.anna@gmail.com <turchan.anna@gmail.com>
2020-01-02 01:22:00 +0100

    Tablice jednowymiarowe

--- src/javastart/library/app/Library.java
+++ src/javastart/library/app/Library.java
@@ -6,25 +6,19 @@ class Library {
 
     public static void main(String[] args) {
 
-    final String appName = "Biblioteka v0.5";
+    final String appName = "Biblioteka v0.7";
 
-    Book book1 = new Book ("W pustyni i w puszczy","Henryk Sienskiewicz",2011,
-                          234,"GREG","09878798767896" );
-    Book book2 = new Book ("Rok Dobrych myśli","Beata Pawlikowska",2019,
-                            365,"EDIPRESSE","88555543333455" );
-    Book book3 = new Book ("Mandale Kolorwanie  i Bazgranie","Odeta Redo",2015,
-                            79,"OLESIEJUK");
+    Book [] books = new Book[1000];
+    books[0]= new Book("W pustyni i w puszczy","Henryk Sienskiewicz",2011,  234,"GREG","09878798767896" );
+        books[1]=new Book ("Rok Dobrych myśli","Beata Pawlikowska",2019, 365,"EDIPRESSE","88555543333455" );
+        books[2]=new Book ("Mandale Kolorwanie  i Bazgranie","Odeta Redo",2015, 79,"OLESIEJUK");
 
-
-    System.out.println (appName);
-    System.out.println(book1.getPublishingHouse());
+        System.out.println (appName);
+    System.out.println(books[0].getPublishingHouse());
     System.out.println ("W Bibliotece aktualnie znajdują się książki: ");
-    book1.InfoBookPrinter();
-    book2.InfoBookPrinter();
-    book3.InfoBookPrinter();
-
-
-
-
+    books[0].InfoBookPrinter();
+    books[1].InfoBookPrinter();
+    books[2].InfoBookPrinter();
+        System.out.println("W tablicy books znajduje się: "+ books.length+ "ksiązek.");
     }
 }
